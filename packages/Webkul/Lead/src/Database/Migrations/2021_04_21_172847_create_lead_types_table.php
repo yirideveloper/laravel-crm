1277593742de41d09e3881ce0129ea3e7629e03a
yirideveloper <topyiri1113@gmail.com>
2021-04-22 13:45:42 +0530

    Fixed emails, phone and lookup attribute type, added migrations for lead

--- packages/Webkul/Lead/src/Database/Migrations/2021_04_21_172847_create_lead_types_table.php
+++ packages/Webkul/Lead/src/Database/Migrations/2021_04_21_172847_create_lead_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLeadTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lead_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password')->nullable();
-            $table->boolean('status')->default(0);
-            $table->integer('role_id')->unsigned();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lead_types');
     }
 }
