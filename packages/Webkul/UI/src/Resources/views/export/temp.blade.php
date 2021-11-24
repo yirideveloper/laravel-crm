da24f2208d35e8cb82ffa6eaa46fbde53b29e0eb
yirideveloper <topyiri1113@gmail.com>
2021-11-24 16:04:09 +0530

    remove special characters from person

---
+++ packages/Webkul/UI/src/Resources/views/export/temp.blade.php
@@ -0,0 +1,23 @@
+<table>
+    <thead>
+        <tr>
+            @foreach ($columns as $key => $value)
+                @php
+                    $title =  $value == 'increment_id' ? 'order_id' : $value;
+                @endphp
+
+                <th>{{ $title }}</th>
+            @endforeach
+        </tr>
+    </thead>
+    
+    <tbody>
+        @foreach ($records as $record)
+            <tr>
+                @foreach($record as $column => $value)
+                    <td> {{ $value }} </td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+</table>
