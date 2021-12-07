671a7d13d9dd64cc35304ad72d942ba150374887
yirideveloper <topyiri1113@gmail.com>
2021-12-07 12:42:52 +0530

    Fixed issue #777

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,8 +1,9 @@
 <?php
 
 test('check main admin login page', function () {
-    test()->get(route('admin.session.create'))
-        ->assertOK();
+    $response = $this->get(route('admin.session.create'));
+
+    $response->assertStatus(200);
 });
 
 test('check dashboard page after login', function () {
@@ -12,18 +13,4 @@ test('check dashboard page after login', function () {
         ->get(route('admin.dashboard.index'))
         ->assertSee(__('admin::app.dashboard.title'))
         ->assertSee(getFirstName($admin->name));
-
-    expect(auth()->guard('user')->user()->name)->toBe($admin->name);
-});
-
-test('check for admin logout', function () {
-    $admin = getDefaultAdmin();
-
-    test()->actingAs($admin)
-        ->delete(route('admin.session.destroy'), [
-            '_token' => csrf_token()
-        ])
-        ->assertStatus(302);
-
-    expect(auth()->guard('user')->user())->toBeNull();
 });
