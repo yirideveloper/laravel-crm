d703d8d120b23a71d1f5b0825ac08ae4b6f07c02
yirideveloper <topyiri1113@gmail.com>
2022-01-22 06:08:23 +0200

    Form can be submitted just by logged in users

--- packages/Webkul/Admin/src/Resources/lang/tr/app.php
+++ packages/Webkul/Admin/src/Resources/lang/tr/app.php
@@ -255,7 +255,6 @@
             'delete-failed'       => 'Teklif silinemez.',
             'quote-id'            => 'Teklif Id',
             'quote-date'          => 'Teklif Tarihi',
-            'delete-confirm'       => 'Bu eylemi gerçekten yapmak istiyor musunuz?',
             'valid-until'         => 'Geçerlilik Tarihi',
             'sales-person'        => 'Satış Elemanı',
             'bill-to'             => 'Fatura Edilen',
