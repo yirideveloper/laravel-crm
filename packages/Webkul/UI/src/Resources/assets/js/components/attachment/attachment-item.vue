787fb153d2f07e4ef71db97b5bdb9466084c2ef8
yirideveloper <topyiri1113@gmail.com>
2022-09-13 16:23:06 +0530

    removed unused dependency

--- packages/Webkul/UI/src/Resources/assets/js/components/attachment/attachment-item.vue
+++ packages/Webkul/UI/src/Resources/assets/js/components/attachment/attachment-item.vue
@@ -2,10 +2,8 @@
     <div class="attachment-item" v-show="show">
         <span>
             <input
-                :id="attachment.id ?? ''"
-                class="attachment"
                 type="file"
-                name="attachment[]"
+                :name="inputName"
                 @change="attachmentSelected($event)"
                 ref="attachment"
             />
@@ -25,58 +23,42 @@ export default {
 
             required: false,
 
-            default: null,
+            default: null
         },
 
         inputName: {
             type: String,
 
-            required: false,
-        },
+            required: false
+        }
     },
 
-    data: function () {
+    data: function() {
         return {
             show: false,
 
-            name: "",
+            name: ""
         };
     },
 
-    mounted: function () {
+    mounted: function() {
         this.activate();
     },
 
     methods: {
-        activate: function () {
+        activate: function() {
             if (this.attachment.isNew) {
                 this.openFileBrowser();
             } else {
-                if (this.attachment?.type) {
-                    this.addDropZoneFile();
-
-                    return;
-                }
-
                 this.showExistingAttachment();
             }
         },
 
-        openFileBrowser: function () {
+        openFileBrowser: function() {
             this.$refs.attachment.click();
         },
 
-        addDropZoneFile() {
-            let data = new DataTransfer();
-
-            data.items.add(this.attachment.file);
-
-            this.$refs.attachment.files = data.files;
-
-            this.attachmentSelected();
-        },
-
-        attachmentSelected: function () {
+        attachmentSelected: function() {
             let attachment = this.$refs.attachment;
 
             if (attachment.files && attachment.files[0]) {
@@ -86,15 +68,15 @@ export default {
             }
         },
 
-        showExistingAttachment: function () {
+        showExistingAttachment: function() {
             this.show = true;
 
             this.name = this.attachment.name;
         },
 
-        removeAttachment: function () {
+        removeAttachment: function() {
             this.$emit("onRemoveAttachment", this.attachment);
-        },
-    },
+        }
+    }
 };
 </script>
