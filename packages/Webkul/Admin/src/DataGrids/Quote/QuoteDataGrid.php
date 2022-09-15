a9cf9c78fb62142f51fb9c505fa124a7d205e5e7
yirideveloper <topyiri1113@gmail.com>
2022-09-15 10:35:18 +0530

    Issue #663 fixed

--- packages/Webkul/Admin/src/DataGrids/Quote/QuoteDataGrid.php
+++ packages/Webkul/Admin/src/DataGrids/Quote/QuoteDataGrid.php
@@ -72,8 +72,7 @@ class QuoteDataGrid extends DataGrid
                 'users.id as user_id',
                 'users.name as sales_person',
                 'persons.id as person_id',
-                'persons.name as person_name',
-                'quotes.expired_at as expired_quotes'
+                'persons.name as person_name'
             )
             ->leftJoin('users', 'quotes.user_id', '=', 'users.id')
             ->leftJoin('persons', 'quotes.person_id', '=', 'persons.id');
@@ -94,12 +93,6 @@ class QuoteDataGrid extends DataGrid
         $this->addFilter('person_name', 'persons.name');
         $this->addFilter('expired_at', 'quotes.expired_at');
         $this->addFilter('created_at', 'quotes.created_at');
-
-        if (request()->input('expired_quotes.in') == 1) {
-            $this->addFilter('expired_quotes', DB::raw('DATEDIFF(NOW(), ' . DB::getTablePrefix() . 'quotes.expired_at) >= ' . DB::getTablePrefix() . 'NOW()'));
-        } else {
-            $this->addFilter('expired_quotes', DB::raw('DATEDIFF(NOW(), ' . DB::getTablePrefix() . 'quotes.expired_at) < ' . DB::getTablePrefix() . 'NOW()'));
-        }
 
         $this->setQueryBuilder($queryBuilder);
     }
@@ -129,14 +122,6 @@ class QuoteDataGrid extends DataGrid
 
                 return "<a href='" . $route . "'>" . $row->sales_person . "</a>";
             },
-        ]);
-
-        $this->addColumn([
-            'index'            => 'expired_quotes',
-            'label'            => trans('admin::app.datagrid.expired_quotes'),
-            'type'             => 'single_dropdown',
-            'dropdown_options' => $this->getBooleanDropdownOptions('yes_no'),
-            'searchable'       => false,
         ]);
 
         $this->addColumn([
@@ -236,6 +221,13 @@ class QuoteDataGrid extends DataGrid
             'method' => 'GET',
             'route'  => 'admin.quotes.edit',
             'icon'   => 'pencil-icon',
+        ]);
+
+        $this->addAction([
+            'title'  => trans('ui::app.datagrid.print'),
+            'method' => 'GET',
+            'route'  => 'admin.quotes.print',
+            'icon'   => 'sprite quotes-icon',
         ]);
 
         $this->addAction([
